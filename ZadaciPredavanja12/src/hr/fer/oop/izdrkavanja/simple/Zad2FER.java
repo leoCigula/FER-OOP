63aeac9cb41f18241ca000691c0e72d8f180f8ce
leoCigula <109247428+leoCigula@users.noreply.github.com>
2023-03-02 18:32:08 +0100

    Auditorne 1 - zadaci

---
+++ ZadaciPredavanja12/src/hr/fer/oop/izdrkavanja/simple/Zad2FER.java
@@ -0,0 +1,18 @@
+package hr.fer.oop.izdrkavanja.simple;
+
+public class Zad2FER {
+
+	public static void main(String[] args) {
+		double sum = 0;
+		int first5 = (int) (Math.PI * 1e5);
+		int calc5 = 0;		
+		int i=0;
+		while(first5 != calc5) {
+			++i;
+			sum += 1./((double)i*i);
+			calc5 = (int) (1e5 * Math.sqrt(6 * sum));			
+		}
+		System.out.printf("Pi after %d steps is %f%n", i, Math.sqrt(6. * sum));		
+	}
+
+}
